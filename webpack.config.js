8460e21d8d481099c65a4c910a3b7cee5a0705ec
muqsith <muqsithirfan@gmail.com>
2017-11-22 18:34:03 +0400

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path'),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    ExtractTextPlugin = require('extract-text-webpack-plugin')
+    ;
+
+const config = {
+    entry: path.resolve(__dirname, 'src', 'js', 'app.js'),
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: ['css-loader', 'postcss-loader']
+            })
+        }]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "public"),
+        compress: true,
+        port: 8888
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html')
+        }),
+        new ExtractTextPlugin('main.css')
+    ]
+}
+
+module.exports = config;
